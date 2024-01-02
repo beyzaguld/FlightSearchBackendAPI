1e9d2dea49ebd41cc8a844a0da3241988d02ef88
Beyzagül Demir <85847630+beyzaguld@users.noreply.github.com>
2024-01-02 12:06:11 +0300

    Initial upload

---
+++ flightsearch/src/main/java/com/amadeus/entity/Flight.java
@@ -0,0 +1,88 @@
+package com.amadeus.entity;
+
+import jakarta.persistence.Id;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Flight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "departure_airport")
+    private String departureAirport;
+
+    @Column(name = "arrival_airport")
+    private String arrivalAirport;
+
+    @Column(name = "departure_date_time")
+    private LocalDateTime departureDateTime;
+
+    @Column(name = "return_date_time")
+    private LocalDateTime returnDateTime;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    // Default constructor
+    public Flight() {
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public LocalDateTime getDepartureDateTime() {
+        return departureDateTime;
+    }
+
+    public void setDepartureDateTime(LocalDateTime departureDateTime) {
+        this.departureDateTime = departureDateTime;
+    }
+
+    public LocalDateTime getReturnDateTime() {
+        return returnDateTime;
+    }
+
+    public void setReturnDateTime(LocalDateTime returnDateTime) {
+        this.returnDateTime = returnDateTime;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
+
